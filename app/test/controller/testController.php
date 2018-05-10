9696b205717568844ddcd6515fd96d530875685b
clound's MBP <86916264@qq.com>
2018-05-10 17:22:42 +0800

    第一次保存

---
+++ app/test/controller/testController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace app\test\controller;
+
+use app\test\model\CountBaseDatas;
+use cmf\controller\AdminBaseController;
+use think\Controller;
+use think\Request;
+
+class testController extends AdminBaseController
+{
+    /**
+     * 显示资源列表
+     *
+     * @return \think\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * 显示创建资源表单页.
+     *
+     * @return \think\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * 保存新建的资源
+     *
+     * @param  \think\Request $request
+     * @return \think\Response
+     */
+    public function save(Request $request)
+    {
+        //
+    }
+
+    /**
+     * 显示指定的资源
+     *
+     * @param  int $id
+     * @return \think\Response
+     */
+    public function read($id)
+    {
+        //
+    }
+
+    /**
+     * 显示编辑资源表单页.
+     *
+     * @param  int $id
+     * @return \think\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * 保存更新的资源
+     *
+     * @param  \think\Request $request
+     * @param  int $id
+     * @return \think\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * 删除指定资源
+     *
+     * @param  int $id
+     * @return \think\Response
+     */
+    public function delete($id)
+    {
+        //
+    }
+
+    public function test()
+    {
+
+        $model = new CountBaseDatas();
+        $datas = $model->select();
+        $this->assign('data', $datas);
+        return $this->fetch();
+    }
+}
