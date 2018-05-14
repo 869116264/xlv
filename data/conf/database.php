a48e73ba3ae9c61225e02de8a5d2bcf63a61be06
clound's MBP <86916264@qq.com>
2018-05-14 11:08:49 +0800

    添加配置文件

---
+++ data/conf/database.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * 配置文件
+ */
+
+return [
+    // 数据库类型
+    'type'     => 'mysql',
+    // 服务器地址
+    'hostname' => '180.76.53.221',
+    // 数据库名
+    'database' => 'xlcount-view',
+    // 用户名
+    'username' => 'rdsroot',
+    // 密码
+    'password' => 'SYLKLy1n',
+    // 端口
+    'hostport' => '3306',
+    // 数据库编码默认采用utf8
+    'charset'  => 'utf8',
+    // 数据库表前缀
+    'prefix'   => 'xlv_',
+    "authcode" => 'Dz5DU62Xr86cOi3UXO',
+    //#COOKIE_PREFIX#
+];
+
